fc27b95d72756ab6da8be0febca0b8793a4c23b5
PoonpipatSr <PoonpipatSr@users.noreply.github.com>
2025-08-05 17:00:45 +0700

    End array class 1

--- arr.js
+++ arr.js
@@ -1,16 +1,19 @@
 let arr1 = [1, "sfsf", true, 9, false, "sdsds"];
 
-// let i = 0;
-// while (i < arr1.length) {
-//     console.log(`index = ${i} : ${arr1[i]}`);
-//     i++;
-// }
-//
-// console.log("-------------------------");
-// console.log(`array length = ${arr1.length}`);
-// console.log(`Value 1 is ${arr1[arr1.length - arr1.length]} -> index: ${arr1.length - arr1.length}`);
-// console.log(`Value 6 is ${arr1[arr1.length - 1]} -> index: ${arr1.length - 1}`);
+/*
+let i = 0;
+while (i < arr1.length) {
+    console.log(`index = ${i} : ${arr1[i]}`);
+    i++;
+}
 
+console.log("-------------------------");
+console.log(`array length = ${arr1.length}`);
+console.log(`Value 1 is ${arr1[arr1.length - arr1.length]} -> index: ${arr1.length - arr1.length}`);
+console.log(`Value 6 is ${arr1[arr1.length - 1]} -> index: ${arr1.length - 1}`);
+*/
+
+/*
 // grow
 console.log("-------------------------");
 arr1[arr1.length+2] = "Note"
@@ -28,3 +31,67 @@ console.log(`array length = ${arr1.length}`);
 arr1.pop()
 console.log(arr1);
 console.log(`array length = ${arr1.length}`);
+*/
+
+/*
+arr1.splice(0, 2)
+arr1.push([1, true, "Hello"])
+console.log(arr1);
+arr1.push({id: 1, name: "Note"})
+console.log(arr1);
+*/
+
+/*
+//array contains many objects
+const colors = [{ id: 1, name: 'red', tone: 'hot' }, { id: 2, name: 'purple', tone: 'cool' }, { id: 3, name: 'white', tone: 'neutral' }]
+// console.log(colors)
+
+const contacts = [{ email: ['abc@gmail.com', 'foo@kmutt.ac.th'] },
+{ address: ['Bangkok', 'Puket'] }]
+// console.log(contacts)
+
+//array contains many arrays
+const groups = [['Malee', 'Jane'], ['John', 'Jay', 'Pete']]
+// console.log(groups)
+
+// console.log(colors[1]);
+
+// console.log(contacts[0].email[1]);
+
+for (let i = 0; i < groups.length; i++) {
+    let last = groups[i];
+    console.log(last[last.length - 1]);
+}
+*/
+
+/*
+let a = "Hello"
+let b = [...a]
+console.log(...b);
+
+const arr2 = new Array()
+console.log(arr1) // []
+console.log(arr1.length) // 0
+
+const arr3 = new Array(10)
+console.log(arr2) // [10 empty items]
+console.log(arr2.length) // 10
+
+const arr4 = new Array(1, 5, 7, 'hello')
+console.log(arr3) // [ 1, 5, 7, 'hello' ]
+console.log(arr3.length) // 4
+
+const arr5 = Array.of(5)
+console.log(arr5); // [5]
+console.log(arr5.length); // 1
+
+const str = 'Hello'
+const arr6 = Array.from(str)
+console.log(arr6); // [ 'H', 'e', 'l', 'l', 'o' ]
+console.log(arr6.length); // 5
+
+const arr7 = Array.from(arr5)
+console.log(arr7); // [5]
+console.log(arr7.length); // 1
+*/
+
